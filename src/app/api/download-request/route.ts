975001a2b571038bcc2c34ff53dbeb8f108aa279
吉田　侑生 <yoshida_yuusei@yoshidayuuseinoMacBook-Pro.local>
2026-04-29 14:58:12 +0900

    fix: Resendをリクエスト時に初期化してビルドエラーを修正

--- src/app/api/download-request/route.ts
+++ src/app/api/download-request/route.ts
@@ -1,9 +1,8 @@
 import { Resend } from "resend";
 import { NextRequest, NextResponse } from "next/server";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
-
 export async function POST(req: NextRequest) {
+  const resend = new Resend(process.env.RESEND_API_KEY);
   const { company, name, email, tel } = await req.json();
 
   if (!name || !email) {
